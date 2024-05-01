8568bdc3a768f56ab69edba73696f98cd646dfa1
hshehriyaralam <hshehriyaralam@gmail.com>
2024-05-01 13:27:22 -0600

    Color_ganerator

--- Countdown Timer/app.js
+++ Countdown Timer/app.js
@@ -1,10 +1,6 @@
 const endDate = "23 August 2024 12:00:00 AM";
-
-
 document.getElementById('end-date').innerText = endDate;
-
 const inputs = document.querySelectorAll("input");
-
 function clock(){
     const end = new Date(endDate);
     const now = new Date();
@@ -12,18 +8,14 @@ function clock(){
     // console.log(now);
     const diff = (end - now) / 1000;
     // console.log(diff);
-
     if(diff < 0) return;
-
     // Convert into Days
     inputs[0].value = Math.floor(diff / 3600 / 24);
     inputs[1].value = Math.floor(diff / 3600 ) % 24;
     inputs[2].value = Math.floor(diff / 60 ) % 60;
     inputs[3].value = Math.floor(diff) % 60;  
 }
-
 clock();
-
 setInterval(
     () => {
         clock()
